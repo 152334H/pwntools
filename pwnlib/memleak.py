152334H/pwntools

commit 8b6e6ed67df10dd56a739e2a81adda381a0a8ae2

Enhance py2 32-bit archs support
This was a quick `grep -r isinstance | grep '\<int\>'`

pwnlib/memleak.py
@@ -155,17 +155,17 @@ class MemLeak(object):
         Arguments:
             address(int): Base address to calculate offsets from
             field(obj):   Instance of a ctypes field
-            expected(int,str): Expected value
+            expected(int,bytes): Expected value
 
         Return Value:
             The type of the return value will be dictated by
             the type of ``field``.
         """
-        if not isinstance(expected, (int, bytes)):
-            raise TypeError("Expected value must be an int or bytes")
 
-        if isinstance(expected, int):
+        if isinstance(expected, six.integer_types):
             expected = pack(expected, bytes=obj.size)
+        elif not isinstance(expected, bytes):
+            raise TypeError("Expected value must be an int or bytes")
 
         assert obj.size == len(expected)
 

pwnlib/adb/bootloader.py
@@ -64,7 +64,7 @@ class BootloaderImage(object):
         Returns:
             Contents of the image.
         """
-        if isinstance(index_or_name, int):
+        if isinstance(index_or_name, six.integer_types):
             index = index_or_name
         else:
             for i in range(len(self.img_info)):

pwnlib/gdb.py
@@ -399,7 +399,7 @@ def debug(args, gdbscript=None, exe=None, ssh=None, env=None, sysroot=None, **kw
     >>> io.interactive() # doctest: +SKIP
     >>> io.close()
     """
-    if isinstance(args, (int, tubes.process.process, tubes.ssh.ssh_channel)):
+    if isinstance(args, six.integer_types + (tubes.process.process, tubes.ssh.ssh_channel)):
         log.error("Use gdb.attach() to debug a running process")
 
     if isinstance(args, (bytes, six.text_type)):

pwnlib/log.py
@@ -283,7 +283,7 @@ class Logger(object):
         self._logger = logger
 
     def _getlevel(self, levelString):
-        if isinstance(levelString, int):
+        if isinstance(levelString, six.integer_types):
             return levelString
         return logging._levelNames[levelString.upper()]
 

pwnlib/rop/gadgets.py
@@ -54,12 +54,12 @@ class Gadget(object):
 
     def __getitem__(self, key):
         # Backward compatibility
-        if isinstance(key, int):
+        if isinstance(key, six.integer_types):
             key = self.__indices[key]
         return getattr(self, key)
 
     def __setitem__(self, key, value):
         # Backward compatibility
-        if isinstance(key, int):
+        if isinstance(key, six.integer_types):
             key = self.__indices[key]
         return setattr(self, key, value)

pwnlib/rop/ret2dlresolve.py
@@ -65,6 +65,7 @@ You can also use ``Ret2dlresolve`` on AMD64:
     True
 """
 
+import six
 from copy import deepcopy
 
 from pwnlib.context import context
@@ -356,7 +357,7 @@ class Ret2dlresolvePayload(object):
             elif isinstance(top, bytes):
                 top = pack(self.data_addr + len(self.payload) + queue.size())
                 queue.append(MarkedBytes(queue[0]))
-            elif isinstance(top, int):
+            elif isinstance(top, six.integer_types):
                 top = pack(top)
 
             self.payload += top

pwnlib/shellcraft/__init__.py
@@ -155,7 +155,7 @@ class module(ModuleType):
             return hex(n)
 
     def okay(self, s, *a, **kw):
-        if isinstance(s, int):
+        if isinstance(s, six.integer_types):
             s = packing.pack(s, *a, **kw)
         return b'\0' not in s and b'\n' not in s
 

pwnlib/shellcraft/templates/aarch64/linux/stage.asm
@@ -1,4 +1,5 @@
 <%
+import six
 from pwnlib.shellcraft.aarch64 import mov
 from pwnlib.shellcraft.aarch64.linux import read, readn, mmap
 from pwnlib import constants as C
@@ -28,7 +29,7 @@ Example:
     protection = C.PROT_READ | C.PROT_WRITE | C.PROT_EXEC
     flags      = C.MAP_ANONYMOUS | C.MAP_PRIVATE
 
-    assert isinstance(fd, int)
+    assert isinstance(fd, six.integer_types)
 %>
 %if length is None:
     /* How many bytes should we receive? */

commit cef993328e659432e9ada30eb2dab15c518a01cb

Fix memleak regression after 8b6e6ed

pwnlib/memleak.py
@@ -4,6 +4,7 @@ from __future__ import division
 import ctypes
 import functools
 import string
+import six
 
 from six.moves import range
 