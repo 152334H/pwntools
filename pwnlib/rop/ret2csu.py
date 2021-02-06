12743be7122b7d90ecdf94de04027d05c9592dc2
152334H <54623771+152334H@users.noreply.github.com>
2021-02-06 23:38:05 +0800

    apply ctf patch

--- pwnlib/rop/ret2csu.py
+++ pwnlib/rop/ret2csu.py
@@ -56,33 +56,22 @@ def ret2csu(rop, elf, edi, rsi, rdx, rbx, rbp, r12, r13, r14, r15, call=None):
     csu_function = elf.read(elf.sym['__libc_csu_init'], elf.sym['__libc_csu_fini'] - elf.sym['__libc_csu_init'])
 
     # 1st gadget: Populate registers in preparation for 2nd gadget
-    for insn in md.disasm(csu_function, elf.sym['__libc_csu_init']):
+    for i,insn in enumerate(md.disasm(csu_function, elf.sym['__libc_csu_init'])):
         if insn.mnemonic == 'pop' and insn.operands[0].reg == X86_REG_RBX:
             rop.raw(insn.address)
             break
+        if insn.mnemonic == 'call': call_index = i
     # rbx and rbp must be equal after 'add rbx, 1'
     rop.raw(0x00)  # pop rbx
     rop.raw(0x01)  # pop rbp
-    if call:
-        rop.raw(call)  # pop r12
-    else:
-        rop.raw(fini)  # pop r12
 
     # Older versions of gcc use r13 to populate rdx then r15d to populate edi, newer versions use the reverse
     # Account for this when the binary was linked against a glibc that was built with a newer gcc
-    for insn in md.disasm(csu_function, elf.sym['__libc_csu_init']):
-        if insn.mnemonic == 'mov' and insn.operands[0].reg == X86_REG_RDX and insn.operands[1].reg == X86_REG_R13:
-            rop.raw(rdx)  # pop r13
-            rop.raw(rsi)  # pop r14
-            rop.raw(edi)  # pop r15
-            rop.raw(insn.address)
-            break
-        elif insn.mnemonic == 'mov' and insn.operands[0].reg == X86_REG_RDX and insn.operands[1].reg == X86_REG_R15:
-            rop.raw(edi)  # pop r13
-            rop.raw(rsi)  # pop r14
-            rop.raw(rdx)  # pop r15
-            rop.raw(insn.address)
-            break
+    reg_mapping = dict(reversed(list(map(lambda op: insn.reg_name(op.reg)[:3], insn.operands))) for insn in list(md.disasm(csu_function, elf.sym['__libc_csu_init']))[call_index-3:call_index])
+    for reg in ['r12', 'r13', 'r14']: rop.raw(eval(reg_mapping[reg]))
+    if call: rop.raw(call)  # pop r15
+    else: rop.raw(fini)  # pop r15
+    rop.raw(list(md.disasm(csu_function, elf.sym['__libc_csu_init']))[call_index-3].address)
 
     # 2nd gadget: Populate edi, rsi & rdx. Populate optional registers
     rop.raw(Padding('<add rsp, 8>'))  # add rsp, 8
